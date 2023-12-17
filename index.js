257bbf8710f755bccf0294a975d984c4072fd3b2
Azeem Shaik <azeemshaik025@gmail.com>
2023-12-17 16:04:19 +0530

    Final Commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(cors());
 app.use(express.static(path.join(__dirname + "/public")));
 app.set('view engine', 'ejs');
